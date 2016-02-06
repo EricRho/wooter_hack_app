2fe3b75ec1b0f3b2ad2174f97e16b14baaa8d305
EricRho <eric.rho.87@gmail.com>
2016-02-06 03:43:01 -0500

    Remove unused code. Changing so Carlos can start front end

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,34 +11,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 			redirect_to new_user_registration_url
 		end
 	end
-
-	# def google_oauth2
-	# 	auth_hash = request.env['omniauth.auth']
-	# 	uid = auth.hash['uid']
-	# 	name = auth_hash['info']['name']
-	# 	auth = Authorization.find_by_provider_and_uid('google_oauth2', uid)
-
-	# 	if auth
-	# 		user = auth.user
-	# 	else
-	# 		unless current_user
-	# 			unless user = User.find_by_name(name)
-	# 				user = User.create({
-	# 					name: name,
-	# 					password: Devise.friendly_token[0,20],
-	# 					email: "#{UUIDTools::UUID.random_create}@somebullshit.com"
-	# 					})
-	# 	end
-	# 	unless auth= user.authorizations.find_by_provider('google_oauth2')
-	# 		auth = user.authorizations.build(provider: 'google_oauth2', uid: uid)
-	# 		user.authorizations << auth
-	# 	end
-	# 	auth.update_attributes({
-	# 		uid: uid,
-	# 		token: auth_hash['credentials']['token'],
-	# 		secret: auth_hash['credentials']['secret'],
-	# 		name: name,
-	# 		url:
-	# 		})
-	# end
 end
