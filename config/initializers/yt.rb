49d277b766c739a387ff76b146464c1ecd66212c
EricRho <eric.rho.87@gmail.com>
2016-02-08 01:10:08 -0500

    Connect to Youtube API

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,3 +1,10 @@
+# Yt.configure do |config|
+# 	config.api_key = 'AIzaSyAfMU1MEL-nPEE4TU9z40UwHaac7PEtXNM'
+# end
+
+
+# If no account access token
 Yt.configure do |config|
-	config.api_key = 'AIzaSyAfMU1MEL-nPEE4TU9z40UwHaac7PEtXNM'
+	config.client_id = '348959413513-h8khqmjb7pirj69381scb6sh55k1strv.apps.googleusercontent.com'
+	config.client_secret = 'TmbiEsq8PFMl03RTK4NN_6qy'
 end
